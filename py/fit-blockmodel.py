b655391399863f2c7b663fa1c10633252f69380a
Peter Straka <p.straka@unsw.edu.au>
2018-03-18 23:26:58 +1100

    seriously...

--- py/fit-blockmodel.py
+++ py/fit-blockmodel.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     
     entropies = list(map(lambda x: x.entropy(), candidate_models))
     print("Entropies:", entropies)
-    keep = np.argmax(entropies)
+    keep = np.argmin(entropies)
     
     state = candidate_models[keep]
     state = mbs.extract_PPCs(state)
